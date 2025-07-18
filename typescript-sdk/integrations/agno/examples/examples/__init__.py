c8ca26e377a66bc6267cf59047582d869c2ddf73
Markus Ecker <markus.ecker@gmail.com>
2025-07-18 14:40:06 +0200

    update agno example

--- typescript-sdk/integrations/agno/examples/examples/__init__.py
+++ typescript-sdk/integrations/agno/examples/examples/__init__.py
@@ -10,25 +10,34 @@ from agno.models.openai import OpenAIChat
 from agno.tools import tool
 
 
-@tool(
-)
+@tool()
 def generate_haiku(english: List[str], japanese: List[str]) -> str: # pylint: disable=unused-argument
     """
     Generate a haiku in Japanese and its English translation.
+    YOU MUST PROVIDE THE ENGLISH HAIKU AND THE JAPANESE HAIKU.
 
     Args:
-        english: List[str]: An array of three lines of the haiku in English
-        japanese: List[str]: An array of three lines of the haiku in Japanese
+        english: List[str]: An array of three lines of the haiku in English. YOU MUST PROVIDE THE ENGLISH HAIKU.
+        japanese: List[str]: An array of three lines of the haiku in Japanese. YOU MUST PROVIDE THE JAPANESE HAIKU.
 
     Returns:
-        str: A string containing the haiku in Japanese and its English translation
-    """
-    print(english, japanese, flush=True)
+        str: A confirmation message.
+    """ # pylint: disable=line-too-long
     return "Haiku generated"
+
+
+@tool(external_execution=True)
+def change_background(background: str) -> str: # pylint: disable=unused-argument
+    """
+    Change the background color of the chat. Can be anything that the CSS background attribute accepts. Regular colors, linear of radial gradients etc.
+
+    Args:
+        background: str: The background color to change to. Can be anything that the CSS background attribute accepts. Regular colors, linear of radial gradients etc.
+    """ # pylint: disable=line-too-long
 
 agent = Agent(
     model=OpenAIChat(id="gpt-4o"),
-    tools=[generate_haiku],
+    tools=[generate_haiku, change_background],
     description="You are a helpful assistant that can help with tasks and answer questions.",
 )
 
@@ -36,7 +45,7 @@ agui_app = AGUIApp(
   agent=agent,
   name="Agno Agent",
   app_id="agno_agent",
-  description="An helpful assistant.",
+  description="A helpful assistant.",
 )
 
 app = agui_app.get_app()
