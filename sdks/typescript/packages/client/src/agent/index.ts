b51fbda6b4c2d7b8c4de503b37b54a722faaacdf
Markus Ecker <markus.ecker@gmail.com>
2025-09-18 16:05:24 +0200

    v0.0.38-alpha.2

--- sdks/typescript/packages/client/src/agent/index.ts
+++ sdks/typescript/packages/client/src/agent/index.ts
@@ -2,4 +2,8 @@ export { AbstractAgent } from "./agent";
 export type { RunAgentResult } from "./agent";
 export { HttpAgent } from "./http";
 export type { AgentConfig, HttpAgentConfig, RunAgentParameters } from "./types";
-export type { AgentSubscriber, AgentStateMutation, AgentSubscriberParams} from "./subscriber";
+<<<<<<< HEAD:sdks/typescript/packages/client/src/agent/index.ts
+export type { AgentSubscriber, AgentStateMutation, AgentSubscriberParams } from "./subscriber";
+=======
+export type { AgentSubscriber, AgentStateMutation, AgentSubscriberParams } from "./subscriber";
+>>>>>>> 954cfdc (v0.0.38-alpha.2):typescript-sdk/packages/client/src/agent/index.ts
