37c8f10eaa88707ce6dd3d26266af6b7e57016eb
Markus Ecker <markus.ecker@gmail.com>
2025-11-03 14:08:20 +0100

    add maxVersion to existing agents to ensure backward compatibility

--- integrations/adk-middleware/typescript/src/index.ts
+++ integrations/adk-middleware/typescript/src/index.ts
@@ -1,3 +1,7 @@
 import { HttpAgent } from "@ag-ui/client";
 
-export class ADKAgent extends HttpAgent {}
+export class ADKAgent extends HttpAgent {
+  public override get maxVersion(): string {
+    return "0.0.39";
+  }
+}
