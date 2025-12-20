3815d73fcfc5667f50704580583b18fba9816110
Brandon McConnell <brandon@dreamthinkbuild.com>
2025-12-20 02:25:49 -0500

    chore: remove redundant exports

--- sdks/typescript/packages/client/src/index.ts
+++ sdks/typescript/packages/client/src/index.ts
@@ -9,6 +9,3 @@ export * from "./compact";
 export * from "@ag-ui/core";
 export * from "./chunks";
 export * from "./middleware";
-
-export { Middleware, FilterToolCallsMiddleware } from "./middleware";
-export type { MiddlewareFunction } from "./middleware";
