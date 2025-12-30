c381b62b2fbf8085909d4ec86059dea72d4b651e
Dao Hoang Son <daohoangson@gmail.com>
2025-12-30 11:59:34 +0700

    refactor(client): Optimize stringify usages in runSubscribersWithMutation

--- sdks/typescript/packages/client/src/agent/subscriber.ts
+++ sdks/typescript/packages/client/src/agent/subscriber.ts
@@ -222,8 +222,8 @@ export async function runSubscribersWithMutation(
   }
 
   return {
-    ...(JSON.stringify(messages) !== JSON.stringify(initialMessages) ? { messages } : {}),
-    ...(JSON.stringify(state) !== JSON.stringify(initialState) ? { state } : {}),
+    ...(messages !== messages0 ? { messages } : {}),
+    ...(state !== state0 ? { state } : {}),
     ...(stopPropagation !== undefined ? { stopPropagation } : {}),
   };
 }
