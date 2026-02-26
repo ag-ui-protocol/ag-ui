26c4bfb5db84179404cf16df2d45dd144c21e88a
Son Le <tran.minh.son.le@oracle.com>
2026-02-26 07:53:35 +0100

    fix [AG-UI Agent Spec]: fix bugs with wayflow runtime

--- integrations/agent-spec/python/ag_ui_agentspec/runtimes/wayflow_runner.py
+++ integrations/agent-spec/python/ag_ui_agentspec/runtimes/wayflow_runner.py
@@ -1,4 +1,4 @@
-import asyncio
+import logging
 from typing import Any, Dict
 
 from wayflowcore import Flow as WayflowFlow
@@ -9,6 +9,7 @@ from wayflowcore.messagelist import Message, MessageType, ToolRequest, ToolResul
 
 from ag_ui.core import RunAgentInput
 from ag_ui_agentspec.agentspec_tracing_exporter import EVENT_QUEUE
+
 
 def prepare_wayflow_agent_input(input_data: RunAgentInput) -> Dict[str, Any]:
     messages = [m.model_dump() for m in input_data.messages]
@@ -52,37 +53,29 @@ def prepare_wayflow_flow_input(input_data: RunAgentInput) -> Dict[str, Any]:
 
 async def run_wayflow(agent: Any, input_data: RunAgentInput) -> None:
     current_queue = EVENT_QUEUE.get()
-
     if isinstance(agent, WayflowAgent):
         agent._add_talk_to_user_tool = False
         agent._update_internal_state()
         agent_input = prepare_wayflow_agent_input(input_data)
-
-        def _invoke_with_context(fi):
-            token = EVENT_QUEUE.set(current_queue)
-            try:
-                with register_event_listeners([AgentSpecEventListener()]):
-                    conversation = agent.start_conversation(messages=fi)
-                    conversation.execute()
-            finally:
-                EVENT_QUEUE.reset(token)
-
-        await asyncio.to_thread(_invoke_with_context, agent_input)
-
+        token = EVENT_QUEUE.set(current_queue)
+        try:
+            with register_event_listeners([AgentSpecEventListener()]):
+                conversation = agent.start_conversation(messages=agent_input)
+                await conversation.execute_async()
+        except Exception as e:
+            logging.error(f"[AG-UI Agent Spec] WayFlow Agent Error: {repr(e)}")
+        finally:
+            EVENT_QUEUE.reset(token)
     elif isinstance(agent, WayflowFlow):
         flow_input = prepare_wayflow_flow_input(input_data)
-
-        def _invoke_with_context(fi):
-            token = EVENT_QUEUE.set(current_queue)
-            try:
-                with register_event_listeners([AgentSpecEventListener()]):
-                    conversation = agent.start_conversation(fi)
-                    _ = conversation.execute()
-            finally:
-                EVENT_QUEUE.reset(token)
-
-        await asyncio.to_thread(_invoke_with_context, flow_input)
-
+        token = EVENT_QUEUE.set(current_queue)
+        try:
+            with register_event_listeners([AgentSpecEventListener()]):
+                conversation = agent.start_conversation(flow_input)
+                _ = conversation.execute()
+        except Exception as e:
+            logging.error(f"[AG-UI Agent Spec] WayFlow Flow Error: {repr(e)}")
+        finally:
+            EVENT_QUEUE.reset(token)
     else:
-        raise NotImplementedError("Unsupported Wayflow component type")
-
+        raise NotImplementedError("Unsupported WayFlow component type")
